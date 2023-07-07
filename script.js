f0678035e5bf9fec712f9edf76b25b80adce5222
dianafabela8 <fabeladiana.df@gmail.com>
2023-07-07 15:45:17 -0700

    added characters to js

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ var confirmNumber;
 var confirmSpecial;
 var userChoices;
 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var special = ["!", "@", "#", "$", "%", "&'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "[", "]", " ^ ", "_", ];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
