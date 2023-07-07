3e58ed2c27469f5deb2706f21df2807c26a8e574
dianafabela8 <fabeladiana.df@gmail.com>
2023-07-07 15:54:43 -0700

    added comments in js

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ var confirmNumber;
 var confirmSpecial;
 var userChoices;
 
+//alphabet variable
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var blankUpper = [];
+
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+
+upperCase = lowerCase.map(toUpper);
+
+//numver variable
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+//special character variable
 var special = ["!", "@", "#", "$", "%", "&'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "[", "]", " ^ ", "_", ];
 
 // Write password to the #password input
